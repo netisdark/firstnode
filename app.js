5b438097eedd1a4848586cd27eb208bbcf42ea00
Daniel Lepcha <pergap3@gmail.com>
2025-05-28 10:19:02 +0545

    added notifications

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const notifier = require('node-notifier');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static frontend files
 app.use(express.static('public'));
 
-// Socket.io real-time handling
 io.on('connection', (socket) => {
   console.log('A user connected');
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg); // broadcast to everyone
+    io.emit('chat message', msg);
+    notifier.notify({
+    title: 'Realtime chat notification',
+    message: msg
+});
   });
 
   socket.on('disconnect', () => {
