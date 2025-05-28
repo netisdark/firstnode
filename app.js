aab09505ed9321dfc5dc7492244678bc979f71a6
Daniel Lepcha <pergap3@gmail.com>
2025-05-28 10:31:06 +0545

    added username and improved notifications

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-const notifier = require('node-notifier');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+app.use(express.static('public')); // Serves files from the public/ directory
 
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
-    notifier.notify({
-    title: 'Realtime chat notification',
-    message: msg
-});
+  socket.on('chat message', ({ username, message }) => {
+    const fullMessage = `${username}: ${message}`;
+    io.emit('chat message', fullMessage);
   });
 
   socket.on('disconnect', () => {
